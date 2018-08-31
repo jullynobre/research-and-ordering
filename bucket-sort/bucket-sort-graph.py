d661b104002993a9baad3a4051a24b66922a86e4
jullynobre <jullynobre1@gmail.com>
2018-08-31 17:30:35 -0300

    Add Bucket Sort

---
+++ bucket-sort/bucket-sort-graph.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+from random import randint
+import timeit
+
+def desenhaGrafico(x,y,xl = "Entradas", yl = "Saídas"):
+    plt.plot(x,y, label = "Melhor Tempo")
+    plt.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.show()  
+
+def generateList(size):
+    list = []
+    while size > 0:
+        n = randint(1, size)
+        list.append(n)
+        size -= 1
+    return list
+
+def generate_decreasing_list(size):
+    list = []
+    while size > 0: 
+        list.append(size)
+        size -= 1
+    return list
+
+def bucket_sort(alist):
+    largest = max(alist)
+    length = len(alist)
+    size = largest/length
+ 
+    buckets = [[] for _ in range(length)]
+    for i in range(length):
+        j = int(alist[i]/size)
+        if j != length:
+            buckets[j].append(alist[i])
+        else:
+            buckets[length - 1].append(alist[i])
+ 
+    for i in range(length):
+        insertion_sort(buckets[i])
+ 
+    result = []
+    for i in range(length):
+        result = result + buckets[i]
+ 
+    return result
+
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+    
+size = [1000, 20000, 40000, 60000, 80000, 100000]
+time = []
+
+for s in size:
+    time.append(timeit.timeit("bucket_sort({})".format(generate_decreasing_list(s)), setup="from __main__ import bucket_sort", number=1))
+    print(s)
+
+desenhaGrafico(size, time, "Numbers", "Time")
---
+++ bucket-sort/bucket-sort.py
@@ -0,0 +1,32 @@
+def bucket_sort(alist):
+    largest = max(alist)
+    length = len(alist)
+    size = largest/length
+ 
+    buckets = [[] for _ in range(length)]
+    for i in range(length):
+        j = int(alist[i]/size)
+        if j != length:
+            buckets[j].append(alist[i])
+        else:
+            buckets[length - 1].append(alist[i])
+ 
+    for i in range(length):
+        insertion_sort(buckets[i])
+ 
+    result = []
+    for i in range(length):
+        result = result + buckets[i]
+ 
+    return result
+
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+
+print(bucket_sort([7, 2, 9, 1, 0, 4, 7, 6, 2, 8, 9, 1, 2, 4 , 1]))
